b22a5b2a57254ac156355016da15a691f3434951
API2455 <abhay.s.gautam@apisero.com>
2021-11-13 11:25:08 +0530

    This is First commit

---
+++ Strings.java
@@ -0,0 +1,14 @@
+package Strings;
+public class Solution{
+	public static void main(String[] args){
+		String s1="Hi";
+		System.out.println(s1);
+		StringBuffer s2=new StringBuffer("Hello");
+		s2.append("World");
+		System.out.println(s2);
+
+		StringBuilder s3= new StringBuilder("Hello");
+		s3.append("Apisero");
+		System.out.println(s3);
+	}
+}
